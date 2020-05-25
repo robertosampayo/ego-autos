57a43339078a5f809c4507499591d6e3ba0c769e
robertosampayo <jrobertosampayo@gmail.com>
2020-05-25 13:46:37 -0300

    Fix app

--- pages/_app.js
+++ pages/_app.js
@@ -3,12 +3,14 @@ import App from 'next/app'
 import Head from 'next/head'
 import 'swiper/css/swiper.css'
 import global from '../styles/global'
+import { CSSPlugin, gsap } from 'gsap'
 // import { ModelProvider } from '../providers/ModelProvider'
 
 
 class MyApp extends App {
     render() {
         const { Component, pageProps } = this.props;
+        gsap.registerPlugin(CSSPlugin)
         return (
 
                 // <ModelProvider>
